6782085d58aa43841eb10c40832d7e421612909d
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-05-22 07:07:22 +0530

    create student grps function

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const APP = express();
+
+//middleware
+APP.use(express.json());
+
+// Require the routers
+const router = require("./routes/studentGrpsRoute");
+
+
+
+//Use routes
+APP.use("/studentGrps", router);
+
+
 
 const PORT = 8000;
 
@@ -9,7 +21,7 @@ const DB_URL = 'mongodb+srv://project01:project01@afproject.n2ih4.mongodb.net/AF
 
 mongoose.connect(DB_URL)
 .then(()=>{
-    console.log('DB Connected');
+    console.log('DB Connected'); 
 })
 .catch((err)=>console.log('DB connection error',err));
 
