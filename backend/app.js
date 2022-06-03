a8bb427c8b2ae87e98dd85c5ccef90605eed60ee
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-06-03 14:34:53 +0530

    2nd commit update appp.js

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,21 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require('cors');
 
 const errorMiddleware = require("./middleware/error");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //route imports
 const user = require("./routes/userRoute");
+const router = require("./routes/studentGrpsRoute");
 
 app.use("/api/v1", user);
+app.use("/studentGrps", router);
 
 //middleware for errors
 app.use(errorMiddleware);
