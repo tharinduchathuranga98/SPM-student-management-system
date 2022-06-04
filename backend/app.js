029ca8352f6ac1216d95fe7a9cb6e73000f45729
tharinduChathutanga <tchathurangaedu@gmail.com>
2022-06-04 10:17:33 +0530

    all commits

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
@@ -37,6 +42,12 @@ app.use("/api/v1", postRoutesSupervisor);
 app.use("/api/v1", postRoutesCoSupervisor);
 
 app.use("/studentGrps", router);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 //middleware for errors
 app.use(errorMiddleware);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-
 const app = require("./app");
 const { default: mongoose } = require("mongoose");
-const dotenv = require("dotenv");
 
 //config
-dotenv.config({ path: "backend/config/config.env" });
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 const DB_URL =
   "mongodb+srv://project01:project01@afproject.n2ih4.mongodb.net/AF_Project?retryWrites=true&w=majority";
 
@@ -18,4 +18,8 @@ mongoose
   .then(() => {
     console.log("DB Connected");
   })
+
   .catch((err) => console.log("DB connection error", err));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
